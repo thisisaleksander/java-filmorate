900120687fb37e2600b0f9a6577eedea6c88728e
alexander <alexandersizov01@gmail.com>
2023-03-28 13:35:29 +0300

    DONE User and Film models

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,4 +1,17 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.Duration;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
 public class Film {
+    private final int id;
+    private final String name;
+    private String description;
+    private final Date releaseDate;
+    private final Duration duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,4 +1,16 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
 public class User {
+    private final int id;
+    private String email;
+    private String login;
+    private final String name;
+    private final LocalDate birthday;
 }
