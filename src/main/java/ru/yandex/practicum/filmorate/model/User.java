d647800a422c57c5bf587e2a4fb249977ec11da4
Alexander Sizov <91897315+thisisaleksander@users.noreply.github.com>
2023-03-31 13:18:55 +0300

    Update User.java

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,16 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
 
+import javax.validation.constraints.Email;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 public class User {
-    @NonNull
     private int id;
     @Email
     private String email;
