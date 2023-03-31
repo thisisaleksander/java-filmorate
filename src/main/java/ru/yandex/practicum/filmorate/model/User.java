13826dc1ce55abf597cbe91f664097172cb294e8
Alexander Sizov <91897315+thisisaleksander@users.noreply.github.com>
2023-03-31 11:06:15 +0300

    Update User.java

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -18,7 +18,7 @@ public class User {
     private String name;
     private LocalDate birthday;
 
-    public User (String email, String login, String name, LocalDate birthday) {
+    public User(String email, String login, String name, LocalDate birthday) {
         this.email = email;
         this.login = login;
         this.name = name;
