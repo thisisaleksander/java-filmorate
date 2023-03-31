100af4286d5a7c6940df3360156a8079afcd03c8
Alexander Sizov <91897315+thisisaleksander@users.noreply.github.com>
2023-03-31 13:19:32 +0300

    Update Film.java

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -10,7 +9,6 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 public class Film {
-    @NonNull
     private int id;
     private String name;
     private String description;
