98b701ad9b543e6716be35a379980003d9551029
alexander <alexandersizov01@gmail.com>
2023-04-03 16:50:53 +0300

    Update User.java

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -20,4 +20,11 @@ public class User {
         this.name = name;
         this.birthday = birthday;
     }
+
+    public User(String login, String email, LocalDate birthday) {
+        this.email = email;
+        this.login = login;
+        this.name = login;
+        this.birthday = birthday;
+    }
 }
