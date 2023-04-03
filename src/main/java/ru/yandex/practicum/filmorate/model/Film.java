6062c27568b296303c3d514823d525691aefb47e
alexander <alexandersizov01@gmail.com>
2023-04-03 17:08:56 +0300

    Update Film.java

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -12,11 +12,4 @@ public class Film {
     private String description;
     private LocalDate releaseDate;
     private Duration duration;
-
-    public Film(String name, String description, LocalDate releaseDate, Duration duration) {
-        this.name = name;
-        this.description = description;
-        this.releaseDate = releaseDate;
-        this.duration = duration;
-    }
 }
