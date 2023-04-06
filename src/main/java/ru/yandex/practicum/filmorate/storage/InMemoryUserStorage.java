6f198e965bb1bf6bb7ba99bce491ef10900b6d4b
alexander <alexandersizov01@gmail.com>
2023-04-06 12:17:39 +0300

    New UserStorage.java and InMemoryUserStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -1,4 +1,67 @@
 package ru.yandex.practicum.filmorate.storage;
 
-public class InMemoryUserStorage {
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.AlreadyExistException;
+import ru.yandex.practicum.filmorate.exception.DoNotExistException;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.ValidateService;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Slf4j
+@Component
+public class InMemoryUserStorage implements UserStorage {
+    private static final Map<Integer, User> users = new HashMap<>();
+    private int userId = 0;
+    private static final String USER_LOG = "USER - {} : {}, user id = {}";
+
+    @Override
+    public User add(@NonNull User user) {
+        ValidateService.validateUser(user);
+        userId++;
+        user.setId(userId);
+        if (users.containsKey(user.getId())) {
+            throw new AlreadyExistException(String.format(
+                    "User with id = %s already exists",
+                    user.getId()
+            ));
+        }
+        users.put(user.getId(), user);
+        log.info(USER_LOG, LocalDateTime.now(), "registered", user.getId());
+        return users.get(user.getId());
+    }
+
+    @Override
+    public User update(@NonNull int id, @NonNull User user) {
+        ValidateService.validateUser(user);
+        if (users.containsKey(user.getId())) {
+            users.put(user.getId(), user);
+            log.info(USER_LOG, LocalDateTime.now(), "updated", user.getId());
+            return users.get(user.getId());
+        } else {
+            throw new DoNotExistException(String.format(
+                    "User with id = %s do not exists",
+                    user.getId()
+            ));
+        }
+    }
+
+    @Override
+    public User get(@NonNull int id) {
+        return users.get(id);
+    }
+
+    @Override
+    public Set<User> getAll() {
+        log.info("USER - {}, total amount of users: {}", LocalDateTime.now(), users.size());
+        Set<User> setOfUsers = new HashSet<>();
+        users.forEach((key, value) -> setOfUsers.add(value));
+        return setOfUsers;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -1,4 +1,15 @@
 package ru.yandex.practicum.filmorate.storage;
 
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Set;
+
 public interface UserStorage {
+    User add(User user);
+
+    User update(int id, User user);
+
+    User get(int id);
+
+    Set<User> getAll();
 }
