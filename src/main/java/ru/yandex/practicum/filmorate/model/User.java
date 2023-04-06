fa1e2a76d36144518243f075f24f714c999085a7
alexander <alexandersizov01@gmail.com>
2023-04-06 11:50:22 +0300

    New User.java

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.validation.constraints.Email;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Data
 public class User {
@@ -13,4 +14,13 @@ public class User {
     private String login;
     private String name;
     private LocalDate birthday;
+    Set<Long> friends;
+
+    private int countFriends() {
+        return friends.size();
+    }
+
+    public int compareByFriends(User o1, User o2) {
+        return o1.countFriends() - o2.countFriends();
+    }
 }
