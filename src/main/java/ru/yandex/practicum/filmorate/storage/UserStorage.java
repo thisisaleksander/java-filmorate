dc49885f7907428fa73953d8462169a68a0681a6
alexander <alexandersizov01@gmail.com>
2023-04-08 21:42:33 +0300

    Update UserStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -7,9 +7,9 @@ import java.util.Set;
 public interface UserStorage {
     User add(User user);
 
-    User update(int id, User user);
+    User update(long id, User user);
 
-    User get(int id);
+    User get(long id);
 
     Set<User> getAll();
 }
