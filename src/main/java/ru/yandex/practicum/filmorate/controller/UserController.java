f35a8b49ad30637550c4f3cf2dc4c8445868a36f
alexander <alexandersizov01@gmail.com>
2023-04-08 21:20:57 +0300

    Update UserController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,60 +1,60 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.AlreadyExistException;
-import ru.yandex.practicum.filmorate.exception.DoNotExistException;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.service.ValidateService;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 
 import javax.validation.Valid;
-import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Set;
 
 @Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private static final Map<Integer, User> users = new HashMap<>();
-    private int userId = 0;
-    private static final String USER_LOG = "USER - {} : {}, user id = {}";
+    private final InMemoryUserStorage userStorage;
+    private final UserService userService;
 
-    @PostMapping
+    public UserController(InMemoryUserStorage userStorage, UserService userService) {
+        this.userStorage = userStorage;
+        this.userService = userService;
+    }
+
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
     public User createUser(@RequestBody @Valid User user) {
-        ValidateService.validateUser(user);
-        userId++;
-        user.setId(userId);
-        if (users.containsKey(user.getId())) {
-            throw new AlreadyExistException(String.format(
-                    "User with id = %s already exists",
-                    user.getId()
-            ));
-        }
-        users.put(user.getId(), user);
-        log.info(USER_LOG, LocalDateTime.now(), "registered", user.getId());
-        return users.get(user.getId());
+        return userStorage.add(user);
     }
 
-    @PutMapping
+    @PutMapping("/users")
     public User updateUser(@RequestBody @Valid User user) {
-        ValidateService.validateUser(user);
-        if (users.containsKey(user.getId())) {
-            users.put(user.getId(), user);
-            log.info(USER_LOG, LocalDateTime.now(), "updated", user.getId());
-            return users.get(user.getId());
-        } else {
-            throw new DoNotExistException(String.format(
-                    "User with id = %s do not exists",
-                    user.getId()
-            ));
-        }
+        return userStorage.update(user.getId(), user);
     }
 
-    @GetMapping
-    public Collection<User> findAllUsers() {
-        log.info("USER - {}, total amount of users: {}", LocalDateTime.now(), users.size());
-        return users.values();
+    @GetMapping("/users")
+    public Set<User> findAllUsers() {
+        return userStorage.getAll();
+    }
+
+    @PutMapping("/users/{id}/friends/{friendId}")
+    public User addFriend(@PathVariable Integer id, @PathVariable Integer friendId) {
+        return userService.addFriend(id, friendId);
+    }
+
+    @DeleteMapping("/users/{id}/friends/{friendId}")
+    public User removeFriend(@PathVariable Integer id, @PathVariable Integer friendId) {
+        return userService.removeFriend(id, friendId);
+    }
+
+    @GetMapping("/users/{id}/friends")
+    public Set<User> getFriends(@PathVariable Integer id) {
+        return userService.getFriends(id);
+    }
+
+    @GetMapping("/users/{id}/friends/common/{otherId}")
+    public Set<User> getMutualFriends(@PathVariable Integer id, @PathVariable Integer otherId) {
+        return userService.getMutualFriends(id, otherId);
     }
 }
