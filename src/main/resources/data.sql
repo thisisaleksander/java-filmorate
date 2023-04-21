1a4f1e795ba341ed857df5130159b3b1e07f1381
alexander <alexandersizov01@gmail.com>
2023-04-21 09:14:52 +0300

    New updates [9]

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO STATUSES(status_id, status_name)
+INSERT INTO STATUSES(status_name)
 VALUES('request')
 VALUES('active')
 VALUES('deleted');
 
-INSERT INTO GENRES(status_id, status_name)
+INSERT INTO GENRES(genre_name)
 VALUES('Комедия')
 VALUES('Драма')
 VALUES('Триллер')
