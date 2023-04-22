fee24b9fe9f91e897e2cdcb8c9f803d17bd91b96
alexander <alexandersizov01@gmail.com>
2023-04-22 12:06:11 +0300

    Add javadoc

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,11 +6,27 @@ import java.util.Optional;
 import java.util.Set;
 
 public interface FilmStorage {
+    /**
+     * @param film -> film object from json
+     * @return Optional<Film> -> created film object if exists
+     */
     Optional<Film> add(Film film);
 
+    /**
+     * @param id -> id gets from film object
+     * @param film -> film object from json
+     * @return Optional<Film> -> updated film object if exists
+     */
     Optional<Film> update(Integer id, Film film);
 
+    /**
+     * @param id -> parameter from request
+     * @return Optional<Film> -> found film with @param id if exists
+     */
     Optional<Film> get(Integer id);
 
+    /**
+     * @return Set<Film> -> all unique films from table 'films'
+     */
     Set<Film> getAll();
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -6,11 +6,28 @@ import java.util.Optional;
 import java.util.Set;
 
 public interface UserStorage {
+    /**
+     * @param user -> user object from json
+     * @return Optional<User> -> user object that was added
+     */
     Optional<User> add(User user);
 
+    /**
+     * @param id -> gets from film object
+     * @param user -> user object from json
+     * @return Optional<User> -> updated user object if exists
+     */
     Optional<User> update(Integer id, User user);
 
+    /**
+     * @param id -> parameter from request
+     * @return Optional<User> -> found user with @param id if exists
+     */
     Optional<User> get(Integer id);
 
+    /**
+     *
+     * @return Set<User> -> all found users in table 'users'
+     */
     Set<User> getAll();
 }
