963ed9fc65832b6d55f3023d8afbbd99d0b5d4ec
alexander <alexandersizov01@gmail.com>
2023-04-22 12:56:22 +0300

    Add Genre and GenreMapper

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -0,0 +1,25 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Data
+@Entity
+@Builder
+@Component
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Table(name = "GENRES")
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Size(max = 25)
+    private String genre;
+}
