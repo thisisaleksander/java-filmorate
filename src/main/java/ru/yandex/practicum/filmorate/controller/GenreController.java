c6524661f683b21b2d02110a42deb9e9b3b46537
alexander <alexandersizov01@gmail.com>
2023-04-23 18:11:38 +0300

    Fixed problem with genres

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -2,8 +2,6 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.GenreDbStorage;
