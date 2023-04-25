010dfea97857a648547b106cd655570e6358caf2
alexander <alexandersizov01@gmail.com>
2023-04-25 15:52:49 +0300

    Review [1] -> fixed style

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.storage;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.sql.SQLException;
-import java.util.Optional;
 import java.util.Set;
 
 public interface UserStorage {
