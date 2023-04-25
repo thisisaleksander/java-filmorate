d57139966bea61f7e6aaa72acc877b26c01878bd
alexander <alexandersizov01@gmail.com>
2023-04-25 13:13:02 +0300

    Review [1] -> DataSourceAutoConfiguration

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,9 +2,8 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class FilmorateApplication {
 
 	public static void main(String[] args) {
