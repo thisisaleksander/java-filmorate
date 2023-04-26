c4c493d6e7546fae7258ee567139a679a6105947
alexander <alexandersizov01@gmail.com>
2023-04-26 11:21:09 +0300

    Updated FilmMapper

--- src/main/java/ru/yandex/practicum/filmorate/mapper/FilmMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/FilmMapper.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.mapper;
 
 import org.springframework.jdbc.core.RowMapper;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.sql.ResultSet;
@@ -10,7 +9,6 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
 
 public class FilmMapper implements RowMapper<Film> {
     @Override
@@ -29,22 +27,5 @@ public class FilmMapper implements RowMapper<Film> {
                         ),
                 new HashSet<>()
                 );
-    }
-
-    public Film mapRow(ResultSet resultSet, Set<Genre> genres) throws SQLException {
-        return new Film(
-                resultSet.getInt("ID"),
-                resultSet.getString("NAME"),
-                resultSet.getString("DESCRIPTION"),
-                LocalDate.parse(Objects.requireNonNull(resultSet.getString("RELEASE_DATE"))),
-                resultSet.getInt("DURATION"),
-                resultSet.getInt("RATE"),
-                resultSet.getBoolean("DELETED"),
-                new Mpa(
-                        resultSet.getInt("MPA_ID"),
-                        resultSet.getString("MPA_NAME")
-                ),
-                genres
-        );
     }
 }
